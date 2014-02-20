4208861a373cb129d85057a58dd87fc3c31e3cdf
Paul Oliver <puzza007@gmail.com>
2014-02-20 16:29:32 +0100

    Ability to pass HTTP request options to wrap-proxy
    In my case I used it to access self-signed HTTPS
    
    (wrap-proxy "" base-url {:insecure? true})

--- src/tailrecursion/ring_proxy.clj
+++ src/tailrecursion/ring_proxy.clj
@@ -24,7 +24,7 @@
 (defn wrap-proxy
   "Proxies requests to proxied-path, a local URI, to the remote URI at
   remote-uri-base, also a string."
-  [handler ^String proxied-path remote-uri-base]
+  [handler ^String proxied-path remote-uri-base & [http-opts]]
   (wrap-cookies
    (fn [req]
      (if (.startsWith ^String (:uri req) (str proxied-path "/"))
@@ -35,14 +35,14 @@
                                    (subs (:uri req) (.length proxied-path)))
                               nil
                               nil)]
-         (-> {:method (:request-method req)
-              :url (str remote-uri "?" (:query-string req))
-              :headers (dissoc (:headers req) "host" "content-length")
-              :body (if-let [len (get-in req [:headers "content-length"])]
-                      (slurp-binary (:body req) (Integer/parseInt len)))
-              :follow-redirects true
-              :throw-exceptions false
-              :as :stream}
+         (-> (merge {:method (:request-method req)
+                     :url (str remote-uri "?" (:query-string req))
+                     :headers (dissoc (:headers req) "host" "content-length")
+                     :body (if-let [len (get-in req [:headers "content-length"])]
+                             (slurp-binary (:body req) (Integer/parseInt len)))
+                     :follow-redirects true
+                     :throw-exceptions false
+                     :as :stream} http-opts)
              request
              prepare-cookies))
        (handler req)))))
